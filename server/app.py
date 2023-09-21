c1367310be6666b3e404c7f6d8ddaf5e8e55be4f
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-21 23:20:12 +0300

    Mission Passed

--- server/app.py
+++ server/app.py
@@ -20,7 +20,12 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = {
+            
+        }
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
